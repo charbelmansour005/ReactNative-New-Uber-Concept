d871cd36af7b2ffbba5f9a5d8d9ef867ae101a6e
charbelmansour005 <charbelmansour005@gmail.com>
2023-03-27 22:06:39 +0300

    removed <> unused redux slice

--- src/redux/app/store.ts
+++ src/redux/app/store.ts
@@ -2,13 +2,11 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import userReducer from "../login/loginSlice"
 import tourReducer from "../passenger/tour/tourSlice"
 import topBarReducer from "../passenger/topBarSlice"
-import themeReducer from "../theme/themeSlice"
 
 const rootReducer = combineReducers({
   user: userReducer,
   passengertour: tourReducer,
   topBar: topBarReducer,
-  theme: themeReducer,
 })
 
 export type RootState = ReturnType<typeof rootReducer>
